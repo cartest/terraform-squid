2099715199a984025b989d57bcc774ddec4bd7cf
Zordrak <Zordrak@users.noreply.github.com>
2017-03-20 18:19:39 +0000

    Missing healthceck egress rule (#2)

--- sgrules_squid_elb.tf
+++ sgrules_squid_elb.tf
@@ -1,9 +1,19 @@
-# Egress rule on the ELB to allow access to squid ASG on 3128
-resource "aws_security_group_rule" "squid_elb_egress" {
+# Egress rule on the ELB to allow access to squid proxy port
+resource "aws_security_group_rule" "elb_egress_squid_proxy" {
   type                     = "egress"
   protocol                 = "tcp"
-  from_port                = "3128"
-  to_port                  = "3128"
+  from_port                = "${var.proxy_port}"
+  to_port                  = "${var.proxy_port}"
+  security_group_id        = "${aws_security_group.squid_elb_sg.id}"
+  source_security_group_id = "${module.microservice_squid.security_group_id}"
+}
+
+# Egress rule on the ELB to allow access to squid healthcheck port
+resource "aws_security_group_rule" "elb_egress_squid_healthcheck" {
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = "${var.healthcheck_port}"
+  to_port                  = "${var.healthcheck_port}"
   security_group_id        = "${aws_security_group.squid_elb_sg.id}"
   source_security_group_id = "${module.microservice_squid.security_group_id}"
 }
